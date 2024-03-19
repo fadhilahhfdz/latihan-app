051713808b89fb86aa808cf5b2d55f8b27583c83
fadill980 <akungithub878@gmail.com>
2024-03-19 20:05:10 +0700

    update view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/buku', function () {
+    $data = [];
+    $data['poin'] = 83;
+    $data['flag'] = 2;
+    $data['sub_judul'] = 'latihan parsing data di view';
+    $data['buku'] = ['buku 1', 'buku 2', 'buku 3', 'buku 4', 'buku 5'];
+    return view('buku/list', $data);
+});
+
+Route::get('biodata', function () {
+    return view('biodata');
 });
